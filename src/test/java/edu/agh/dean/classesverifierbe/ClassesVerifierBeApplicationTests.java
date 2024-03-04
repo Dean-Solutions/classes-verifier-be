1e439f5cff87de77752e82b127377d983156b31e
Paweł Motyka <pglmotyka@gmail.com>
2024-03-04 21:31:39 +0100

    Init BE repo 🚀

---
+++ src/test/java/edu/agh/dean/classesverifierbe/ClassesVerifierBeApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.agh.dean.classesverifierbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassesVerifierBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
