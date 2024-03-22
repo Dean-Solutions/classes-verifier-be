f1a30edd3ebe4c84097561d0eee3fbab4f9d3300
Adam Mytnik <musikmage@gmail.com>
2024-03-22 19:37:21 +0100

    adding some basic endpoints

--- src/main/java/edu/agh/dean/classesverifierbe/controller/RequestController.java
+++ src/main/java/edu/agh/dean/classesverifierbe/controller/RequestController.java
@@ -1,14 +1,35 @@
 package edu.agh.dean.classesverifierbe.controller;
 
+import edu.agh.dean.classesverifierbe.model.Request;
+import edu.agh.dean.classesverifierbe.model.User;
 import edu.agh.dean.classesverifierbe.service.RequestService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/student")
+@RequestMapping("/request")
 public class RequestController {
     @Autowired
     private RequestService requestService;
+    @GetMapping("/")
+    public ResponseEntity<Page<Request>> getStudents(Pageable pageable,
+                                                     @RequestParam(required = false) String description,
+                                                     @RequestParam(required = false) String status,
+                                                     @RequestParam(required = false) String lastName,
+                                                     @RequestParam(required = false) String submissionDate,
+                                                     @RequestParam(required = false) Integer requestType) {
+//        Page<User> users = requestService.ge(pageable, tag, name, lastName, indexNumber, semester,status);
+//        if(users.isEmpty()){
+//            return ResponseEntity.notFound().build();
+//        }
+//        return ResponseEntity.ok(users);
+        return null;
+    }
 
 }
