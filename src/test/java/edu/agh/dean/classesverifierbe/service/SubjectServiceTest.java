a0bcfa2ef709d179290a8a2fce85ce9378209b38
Adam Biśta <410788@student.agh.edu.pl>
2024-03-23 17:33:51 +0100

    feat(IO-74): add tests for SubjectController and SubjectService

--- src/test/java/edu/agh/dean/classesverifierbe/service/SubjectServiceTest.java
+++ src/test/java/edu/agh/dean/classesverifierbe/service/SubjectServiceTest.java
@@ -1,7 +1,11 @@
 package edu.agh.dean.classesverifierbe.service;
 
+import edu.agh.dean.classesverifierbe.RO.UserRO;
+import edu.agh.dean.classesverifierbe.exceptions.SemesterNotFoundException;
 import edu.agh.dean.classesverifierbe.exceptions.SubjectNotFoundException;
-import edu.agh.dean.classesverifierbe.model.Subject;
+import edu.agh.dean.classesverifierbe.exceptions.SubjectTagAlreadyExistsException;
+import edu.agh.dean.classesverifierbe.model.*;
+import edu.agh.dean.classesverifierbe.model.enums.SemesterType;
 import edu.agh.dean.classesverifierbe.repository.SubjectRepository;
 import edu.agh.dean.classesverifierbe.repository.SubjectTagRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,8 +13,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
@@ -51,5 +62,132 @@ class SubjectServiceTest {
             subjectService.getSubjectById(1L);
         });
     }
+
+
+    @Test
+    void getUsersEnrolledInSubjectForSemesterShouldReturnUsers() throws SubjectNotFoundException, SemesterNotFoundException {
+        Long subjectId = 1L;
+        Long semesterId = 1L;
+
+        Subject subject = mock(Subject.class);
+        User user = User.builder()
+                .userId(1L)
+                .firstName("John")
+                .lastName("Doe")
+                .email("john.doe@example.com")
+                .indexNumber("123456")
+                .build();
+        Enrollment enrollment = new Enrollment();
+        enrollment.setEnrollStudent(user);
+        enrollment.setSemester(new Semester(semesterId, SemesterType.WINTER, 2021, LocalDateTime.now()));
+        Set<Enrollment> enrollments = new HashSet<>();
+        enrollments.add(enrollment);
+        when(subject.getEnrollments()).thenReturn(enrollments);
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+
+        List<UserRO> result = subjectService.getUsersEnrolledInSubjectForSemester(subjectId, semesterId);
+
+
+        verify(subjectRepository).findById(subjectId);
+        assertNotNull(result);
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.size());
+        assertEquals("John", result.get(0).getFirstName());
+        assertEquals("Doe", result.get(0).getLastName());
+    }
+
+
+
+    @Test
+    void shouldFilterSubjectsByTagsAndName() throws SubjectNotFoundException {
+
+        PageRequest pageable = PageRequest.of(0, 10);
+        String tags = "Math,Science";
+        String name = "Algebra";
+
+        Subject subject = new Subject();
+        subject.setName("Algebra 101");
+
+
+        when(subjectRepository.findAll(any(Specification.class), eq(pageable)))
+                .thenReturn(new PageImpl<>(Collections.singletonList(subject)));
+
+
+        Page<Subject> result = subjectService.getAllSubjects(tags, name, pageable);
+
+        verify(subjectRepository).findAll(any(Specification.class), eq(pageable));
+        assertFalse(result.isEmpty());
+        assertEquals(1, result.getContent().size());
+        assertEquals("Algebra 101", result.getContent().get(0).getName());
+    }
+
+    @Test
+    void shouldThrowSubjectNotFoundExceptionWhenNoSubjectsFound() {
+
+        PageRequest pageable = PageRequest.of(0, 10);
+        String tags = "Nonexistent";
+        String name = "Nonexistent";
+
+        when(subjectRepository.findAll(any(Specification.class), eq(pageable)))
+                .thenReturn(Page.empty());
+
+        assertThrows(SubjectNotFoundException.class, () -> {
+            subjectService.getAllSubjects(tags, name, pageable);
+        });
+    }
+
+    @Test
+    void shouldAddTagToSubjectSuccessfully() throws Exception {
+
+        Long subjectId = 1L;
+        Long tagId = 1L;
+
+        Subject subject = Subject.builder()
+                .subjectId(subjectId)
+                .name("Math")
+                .subjectTags(new HashSet<>())
+                .build();
+
+        SubjectTag tag = new SubjectTag(1, "Calculus", "Calculus description", new HashSet<>());
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(subjectTagRepository.findById(tagId)).thenReturn(Optional.of(tag));
+
+
+        subjectService.addTagToSubject(subjectId, tagId);
+
+
+        assertTrue(subject.getSubjectTags().contains(tag));
+        verify(subjectRepository).save(subject);
+    }
+
+    @Test
+    void shouldThrowSubjectTagAlreadyExistsExceptionIfTagAlreadyAssigned() {
+        Long subjectId = 1L;
+        Long tagId = 1L;
+
+        SubjectTag tag = new SubjectTag(1, "Calculus", "Calculus description", new HashSet<>());
+        Set<SubjectTag> tags = new HashSet<>();
+        tags.add(tag);
+
+        Subject subject = Subject.builder()
+                .subjectId(subjectId)
+                .name("Math")
+                .subjectTags(tags)
+                .build();
+
+        when(subjectRepository.findById(subjectId)).thenReturn(Optional.of(subject));
+        when(subjectTagRepository.findById(tagId)).thenReturn(Optional.of(tag));
+
+
+        assertThrows(SubjectTagAlreadyExistsException.class, () -> {
+            subjectService.addTagToSubject(subjectId, tagId);
+        });
+    }
 }
+
+
+
+
 
