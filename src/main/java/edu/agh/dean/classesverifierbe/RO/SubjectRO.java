c5be6ab2be996826e95085ff798f0fc15b1c330c
Adam Biśta <410788@student.agh.edu.pl>
2024-03-23 02:18:05 +0100

    feat(IO-66): add ModelMapperConfig and SubjectRO

---
+++ src/main/java/edu/agh/dean/classesverifierbe/RO/SubjectRO.java
@@ -0,0 +1,20 @@
+package edu.agh.dean.classesverifierbe.RO;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubjectRO {
+    private Long subjectId;
+    private String name;
+    private String description;
+    private Set<SubjectTagRO> subjectTags;
+}
---
+++ src/main/java/edu/agh/dean/classesverifierbe/configuration/ModelMapperConfig.java
@@ -0,0 +1,17 @@
+package edu.agh.dean.classesverifierbe.configuration;
+
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+}
+
