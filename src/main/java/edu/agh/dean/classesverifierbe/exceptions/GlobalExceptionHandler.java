79c8fb4f0c94df1692239ae551731d1db90bb98b
Wojtas2001 <Suski2@op.pl>
2024-04-09 18:59:30 +0200

    IO-89(refactor: remove email validation)

--- src/main/java/edu/agh/dean/classesverifierbe/exceptions/GlobalExceptionHandler.java
+++ src/main/java/edu/agh/dean/classesverifierbe/exceptions/GlobalExceptionHandler.java
@@ -29,7 +29,7 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler({UserAlreadyExistsException.class, UserNotFoundException.class, UserTagAlreadyExistsException.class,
-            UserTagNotFoundException.class, InvalidIndexException.class, InvalidEmailException.class})
+            UserTagNotFoundException.class, InvalidIndexException.class})
     @ResponseBody
     public ResponseEntity<?> handleCustomExceptions(Exception ex) {
         if (ex instanceof UserNotFoundException) {
@@ -44,8 +44,6 @@ public class GlobalExceptionHandler {
         else if(ex instanceof UserTagAlreadyExistsException){
             return new ResponseEntity<>(mapToJson(ex), HttpStatus.CONFLICT);
         } else if (ex instanceof InvalidIndexException) {
-            return new ResponseEntity<>(mapToJson(ex), HttpStatus.FORBIDDEN);
-        } else if (ex instanceof InvalidEmailException) {
             return new ResponseEntity<>(mapToJson(ex), HttpStatus.FORBIDDEN);
         }
         return new ResponseEntity<>(mapToJson(ex), HttpStatus.BAD_REQUEST);
--- src/main/java/edu/agh/dean/classesverifierbe/exceptions/InvalidEmailException.java
+++
@@ -1,7 +0,0 @@
-package edu.agh.dean.classesverifierbe.exceptions;
-
-public class InvalidEmailException extends Exception {
-    public InvalidEmailException(String value) {
-        super("Invalid email: " + value);
-    }
-}
